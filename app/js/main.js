76c83693d9e1be0639dba37201a6e505adf0469b
tramvay5401 <tramvay5401@gmail.com>
2020-01-23 00:29:24 +0200

    end first version

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,20 @@
 $(function() {
- 
+    $('.last__inner').slick({
+        arrows:true,
+        dots:true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+
+      $('.news__inner-wrapper').slick({
+        arrows:true,
+        dots:true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+          
 });
